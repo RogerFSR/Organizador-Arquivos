8bf551f3305792f2b215dbadc0867a948fbe0c37
RogerFSR <rogerfsr2008@outlook.com>
2026-01-23 14:37:37 -0300

    feat: archive organization

--- main.py
+++ main.py
@@ -5,10 +5,39 @@ move,
 ) 
 from extensoes import categorias
 
+archives = list()
+
 sys = os.environ
 
 user_path = input("Insira o caminho do diretório: ")
 os.chdir(user_path)
 
-for k, v in categorias.items():
-    os.mkdir(k)
+for item in os.scandir():
+    if (item.is_file):
+        archives.append(item.name)
+
+try:
+    for k in categorias.keys():
+        os.mkdir(k)
+
+    os.mkdir("Outros")
+
+except(e):
+    print(e)
+
+finally:
+    print("Pastas já estão criadas")
+
+
+for i in range(len(archives)):
+    archive_ext = archives[i].split('.')
+    ext = archive_ext[-1]
+    moved = False
+
+    for k, v in categorias.items():
+        if ext in v:
+            move(archives[i], k)
+            moved = True
+
+    if not moved:
+        move(archives[i], "Outros")
